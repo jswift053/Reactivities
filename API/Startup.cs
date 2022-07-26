cbcb98e089fb404b82ae6207578f84f4d19e241c
Jake Swift <JSwift@onestaffmedical.com>
2022-07-26 09:49:42 -0500

    Security updated

--- API/Startup.cs
+++ API/Startup.cs
@@ -81,7 +81,7 @@ namespace API
             {
                 app.Use(async (context, next) =>
                 {
-                    context.Response.Headers.Add("Strinct-Transport-Security", "max-age=31536000");
+                    context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
                     await next.Invoke();
                 });
             }
